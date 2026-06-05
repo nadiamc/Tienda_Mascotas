20c354cc44486fb336c33dc51584c7c807c974e1
Nadia Medina <nadiacarolinamedina@gmail.com>
2026-06-05 08:34:47 -0300

    fix: agregar ruta principal /

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ArticuloController;
 use App\Http\Controllers\Api\CategoriaController;
 
+Route::get('/', function () {
+    return 'OK';
+});
+
 Route::get('/api/categorias', [CategoriaController::class, 'index']);
 Route::get('/api/articulos', [ArticuloController::class, 'index']);
-
